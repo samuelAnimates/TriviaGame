1b3c3d2f80368b641471eb90e44897bed543212d
smendez92 <samuelremndez14@gmail.com>
2017-05-29 23:00:13 -0500

    timer and question swap laid out

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,3 @@
-var triviaGameVars = {
-
-	questionsArray: [],
-	intervalId: null,
-};
-
 //Object declared with variables and functions to keep track of time/rounds in the trivia game
 var gameClock = {
 
@@ -28,29 +22,24 @@ var gameClock = {
 
   	},
 
-
 };
 
+var triviaGameVars = {
 
-//Declare a question object type comprising everything specific to a question that will appear onscreen
-function question(question, a, b, c, d, answer, gif) {
-    this.questionText = a;
-    this.answerA = b;
-    this.answerB = c;
-    this.answerC = d;
-    this.answerD = answer;
-    this.gifAddress = gif;
+	questionsArray: [],
+	intervalId: null,
+	questionNumber: 0,
 };
 
 //function that creates a new question and loads it into the questions array
 function loadQuestionsIntoArray(){
-	var question0 = new question("question", "a", "b", "c", "d", "answer", "gif");
+	var question0 = new question("question0", "a0", "b", "c", "d", "answer", "gif");
 	triviaGameVars.questionsArray.push(question0);
 
-	var question1 = new question("question", "a", "b", "c", "d", "answer", "gif");
+	var question1 = new question("question1", "a1", "b", "c", "d", "answer", "gif");
 	triviaGameVars.questionsArray.push(question1);
 
-	var question2 = new question("question", "a", "b", "c", "d", "answer", "gif");
+	var question2 = new question("question2", "a2", "b", "c", "d", "answer", "gif");
 	triviaGameVars.questionsArray.push(question2);
 
 	var question3 = new question("question", "a", "b", "c", "d", "answer", "gif");
@@ -76,3 +65,31 @@ function loadQuestionsIntoArray(){
 
 };
 
+function printQuestion(questionNum){
+	
+	//Print question to page
+	var newDiv = $("<div></div>").text(triviaGameVars.questionsArray[questionNum].questionText);
+	$("#question-div").append(newDiv);
+
+	//Print answer choices to page
+	var newDivAnswerA = $("<div class='answer-choice' id='answer-A'></div>").text("A: " + triviaGameVars.questionsArray[questionNum].answerA);
+	$("#answer-choices-div").append(newDivAnswerA);
+	var newDivAnswerB = $("<div class='answer-choice' id='answer-B'></div>").text("B: " + triviaGameVars.questionsArray[questionNum].answerB);
+	$("#answer-choices-div").append(newDivAnswerB);
+	var newDivAnswerC = $("<div class='answer-choice' id='answer-C'></div>").text("C: " + triviaGameVars.questionsArray[questionNum].answerC);
+	$("#answer-choices-div").append(newDivAnswerC);
+	var newDivAnswerD = $("<div class='answer-choice' id='answer-D'></div>").text("D: " + triviaGameVars.questionsArray[questionNum].answerD);
+	$("#answer-choices-div").append(newDivAnswerD);
+
+};
+
+//Declare a question object type comprising everything specific to a question that will appear onscreen
+function question(question, a, b, c, d, answer, gif) {
+    this.questionText = question;
+    this.answerA = a;
+    this.answerB = b;
+    this.answerC = c;
+    this.answerD = d;
+    this.correctAnswer = answer;
+    this.gifAddress = gif;
+};
